98ccde847d608a5c260cd6e57b8484065f176435
Justus Croskery <justus@olmmcc.tk>
2019-09-11 19:48:31 -0400

    Fix url parsing

--- src/lib.rs
+++ src/lib.rs
@@ -4,15 +4,15 @@ use std::net::TcpStream;
 
 pub fn make_secure_request(url: &str) -> String {
     let partial_url = url.split("https://").into_iter().nth(1).unwrap();
-    let mut iter = partial_url.split("/").into_iter();
-    let host = iter.nth(0).unwrap();
-    let location = format!("/{}", iter.nth(1).unwrap_or(""));
+    let mut iter = partial_url.splitn(2, '/').into_iter();
+    let host = iter.next().unwrap();
+    let location = format!("/{}", iter.next().unwrap_or(""));
     let connector = SslConnector::builder(SslMethod::tls()).unwrap().build();
     let stream = TcpStream::connect((host, 443)).unwrap();
     let mut stream = connector.connect(host, stream).unwrap();
     stream
         .write_all(
-            format!(
+            format!(    
                 "GET {} HTTP/1.1\r\nHost: {}\r\nConnection: close\r\n\r\n",
                 location, host
             )
@@ -29,6 +29,6 @@ mod tests {
     use super::*;
     #[test]
     fn it_works() {
-        println!("{}", make_secure_request("https://accounts.google.com/"))
+        println!("{}", make_secure_request("https://accounts.google.com/o/oauth2/v2/auth"))
     }
 }
