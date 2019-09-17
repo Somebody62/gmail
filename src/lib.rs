a577ac1f75b480c984854f8a73e4ca7af3203a6b
Justus Croskery <justus@olmmcc.tk>
2019-09-16 20:34:51 -0400

    Added debug message

--- src/lib.rs
+++ src/lib.rs
@@ -10,18 +10,20 @@ pub fn make_secure_request(url: &str, params: HashMap<&str, &str>) -> String {
     let location = format!("/{}", iter.next().unwrap_or(""));
     let mut body = String::new();
     for (name, value) in params {
-        body += &format!("{} = {}\r\n", name, value);
+        body += &format!("{}={}&", name, value);
     }
+    body = body[0..body.len() - 1].to_string();
     let connector = SslConnector::builder(SslMethod::tls()).unwrap().build();
     let stream = TcpStream::connect((host, 443)).unwrap();
     let mut stream = connector.connect(host, stream).unwrap();
     let message = format!(
-        "POST {} HTTP/1.1\r\nHost: {}\r\nConnection: close\r\nContent-length: {}\r\n\r\n{}",
+        "POST {} HTTP/1.1\r\nHost: {}\r\nConnection: close\r\nContent-length: {}\r\nContent-type: application/x-www-form-urlencoded\r\n\r\n{}",
         location,
         host,
         body.len(),
         body
     );
+    println!("{}", message);
     stream.write_all(message.as_bytes()).unwrap();
     let mut res = vec![];
     stream.read_to_end(&mut res).unwrap();
@@ -34,7 +36,11 @@ mod tests {
     #[test]
     fn it_works() {
         let mut hash = HashMap::new();
-        hash.insert("hi", "bye");
+        hash.insert("response_type", "code");
+        hash.insert("client_id", "596290354919-vbv0declhlln245v40dd2d3qoru2q584.apps.googleusercontent.com");
+        hash.insert("redirect_uri", "https://www.olmmcc.tk/admin/email/");
+        hash.insert("scope", "https://mail.google.com/");
+        hash.insert("access_type", "offline");
         println!(
             "{}",
             make_secure_request("https://accounts.google.com/o/oauth2/v2/auth", hash)
