d5e4a360298493b005558a21594b91c9ebb66866
Justus Croskery <justus@olmmcc.tk>
2019-10-05 10:28:48 -0400

    Got rid of GET, replaced with header

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ use std::collections::HashMap;
 use std::io::prelude::*;
 use std::net::TcpStream;
 
-pub fn make_secure_request(url: &str, params: HashMap<&str, &str>, method: &str) -> String {
+pub fn make_secure_request(url: &str, params: HashMap<&str, &str>, auth: Option<&str>) -> String {
     let partial_url = url.split("https://").into_iter().nth(1).unwrap();
     let mut iter = partial_url.splitn(2, '/').into_iter();
     let host = iter.next().unwrap();
@@ -11,34 +11,26 @@ pub fn make_secure_request(url: &str, params: HashMap<&str, &str>, method: &str)
     let connector = SslConnector::builder(SslMethod::tls()).unwrap().build();
     let stream = TcpStream::connect((host, 443)).unwrap();
     let mut stream = connector.connect(host, stream).unwrap();
-    let message = if method == "GET" {
-        let mut location = location.to_string();
-        location += "?";
-        for (name, value) in params {
-            location += &format!("{}={}&", name, value);
-        }
-        location = location[0..location.len() - 1].to_string();
-        format!(
-            "GET {} HTTP/1.1\r\nHost: {}\r\nConnection: close\r\n\r\n",
-            location,
-            host
-        )
+    let mut body = String::new();
+    for (name, value) in params {
+        body += &format!("{}={}&", name, value);
+    }
+    if body.len() > 0 {
+        body = body[0..body.len() - 1].to_string();
+    }
+    let auth_header = if let Some(a) = auth {
+        format!("Authorization: Bearer {}", a)
     } else {
-        let mut body = String::new();
-        for (name, value) in params {
-            body += &format!("{}={}&", name, value);
-        }
-        if body.len() > 0 {
-            body = body[0..body.len() - 1].to_string();
-        }
-        format!(
-            "POST {} HTTP/1.1\r\nHost: {}\r\nConnection: close\r\nContent-length: {}\r\nContent-type: application/x-www-form-urlencoded\r\n\r\n{}",
-            location,
-            host,
-            body.len(),
-            body
-        )
+        "".to_string()
     };
+    let message = format!(
+        "POST {} HTTP/1.1\r\nHost: {}\r\nConnection: close\r\n{}Content-length: {}\r\nContent-type: application/x-www-form-urlencoded\r\n\r\n{}",
+        location,
+        host,
+        auth_header,
+        body.len(),
+        body
+    );
     stream.write_all(message.as_bytes()).unwrap();
     let mut res = vec![];
     stream.read_to_end(&mut res).unwrap();
@@ -63,7 +55,7 @@ mod tests {
             make_secure_request(
                 "https://www.googleapis.com/gmail/v1/users/me/profile",
                 hash,
-                "GET"
+                None
             )
         )
     }
